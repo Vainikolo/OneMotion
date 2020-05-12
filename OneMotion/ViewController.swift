15021288baea54bd565e9b68afef18d56a7b6452
Jason Vainikolo <jason.vainikolo@gmail.com>
2020-05-12 22:11:22 +1200

    created the launch icon, and the home page

--- OneMotion/ViewController.swift
+++ OneMotion/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var viewProfileButton: UIButton!
+    @IBOutlet weak var addWorkoutButton: UIButton!
+    @IBOutlet weak var createChallengeButton: UIButton!
+    @IBOutlet weak var viewProgressButton: UIButton!
+    @IBOutlet weak var myDayButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        startButton.layer.cornerRadius = startButton.frame.width / 2
+        
+        
+//        viewProfileButton.layer.masksToBounds = false
+//        viewProfileButton.layer.shadowRadius = 10
+//        viewProfileButton.layer.shadowOpacity = 1.0
+//        viewProfileButton.layer.shadowOffset = CGSize(width: 2, height: 2)
+//        viewProfileButton.layer.shadowColor = UIColor.darkGray.cgColor
+        
     }
 
 
