b82d3a2420ecc5d8783ca2ae6fc69d992b87078b
Jason Vainikolo <jason.vainikolo@gmail.com>
2020-05-12 18:37:58 +1200

    Initial Commit

---
+++ OneMotion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OneMotion
+//
+//  Created by Jason Vainikolo on 12/05/20.
+//  Copyright © 2020 Jason Vainikolo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
