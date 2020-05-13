2582352185a99b2785e7954603922cd7ee2044e8
Jason Vainikolo <jason.vainikolo@gmail.com>
2020-05-13 16:39:58 +1200

    new feature: creating a database

--- OneMotion/ProfileViewController.swift
+++ OneMotion/ProfileViewController.swift
@@ -7,18 +7,36 @@
 //
 
 import UIKit
+import SQLite3
 
 class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var saveButton: UIButton!
     @IBOutlet weak var profilePhoto: UIImageView!
+    var db: OpaquePointer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-
         saveButton.layer.cornerRadius = 10.0
+
+
+        let fileURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent("OneMotion.sqlite")
+        
+        if sqlite3_open(fileURL.path, &db) != SQLITE_OK {
+            print("Error Opening database")
+            return
+        }
+        
+        let CreateTableQueuery = "CREATE TABLE IF NOT EXISTS PROFILE(FNAME TEXT PRIMARY KEY, LNAME TEXT, DOB DATE, GENDER TEXT, WEIGHT INTEGER, HEIGHT INTEGER, EMAIL TEXT)"
+        if sqlite3_exec(db, CreateTableQueuery, nil, nil, nil) != SQLITE_OK {
+            print("Error creating table")
+            return
+        }
+        
+        print("everything is fine")
+        
+
     }
     
     @IBAction func changePhotoButton(_ sender: Any) {
