297fe0cfd4f2b7bce58bb2de134e41396cdc1fae
Jason Vainikolo <jason.vainikolo@gmail.com>
2020-05-20 00:12:46 +1200

    new feature: saving data into a table

--- OneMotion/ProfileViewController.swift
+++ OneMotion/ProfileViewController.swift
@@ -4,12 +4,12 @@
 //
 //  Created by Jason Vainikolo on 13/05/20.
 //  Copyright © 2020 Jason Vainikolo. All rights reserved.
-// new edit
+// new edits
 
 import UIKit
 import SQLite3
 
-class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITableViewDelegate {
 
     @IBOutlet weak var firstName: UITextField!
     @IBOutlet weak var lastName: UITextField!
@@ -21,6 +21,7 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
     @IBOutlet weak var saveButton: UIButton!
     @IBOutlet weak var profilePhoto: UIImageView!
     var db: OpaquePointer?
+    var ProfileList = [Profile]()
     
     @IBAction func changePhotoButton(_ sender: Any) {
         
@@ -39,7 +40,7 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
         var Height = height.text?.trimmingCharacters(in: .whitespacesAndNewlines)
         var Gender = gender.text?.trimmingCharacters(in: .whitespacesAndNewlines)
         var Email = email.text?.trimmingCharacters(in: .whitespacesAndNewlines)
-        let ProfilePhoto = profilePhoto.animationImages
+//        let ProfilePhoto = profilePhoto.animationImages
         
         if (fName?.isEmpty)! {
             print("First Name is Empty")
@@ -72,7 +73,7 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
         }
         
         if (Email?.isEmpty)! {
-            Email = nil
+            print("Email is Empty")
             return;
         }
         
@@ -86,37 +87,37 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
         
         if sqlite3_bind_text(stmt, 1, fName, -1, nil) != SQLITE_OK {
             print("Error binding FirstName")
-            return;
+//            return;
         }
         
         if sqlite3_bind_text(stmt, 2, lName, -1, nil) != SQLITE_OK {
             print("Error binding lastName")
-            return;
+//            return;
         }
         
         if sqlite3_bind_text(stmt, 3, DofB, -1, nil) != SQLITE_OK {
             print("Error binding Date of Birth")
-            return;
+//            return;
         }
         
         if sqlite3_bind_int(stmt, 4, (Weight! as NSString).intValue) != SQLITE_OK {
             print("Error binding Weight")
-            return;
+//            return;
         }
         
         if sqlite3_bind_int(stmt, 4, (Height! as NSString).intValue) != SQLITE_OK {
             print("Error binding Height")
-            return;
+//            return;
         }
         
         if sqlite3_bind_text(stmt, 6, Gender, -1, nil) != SQLITE_OK {
             print("Error binding Gender")
-            return;
+//            return;
         }
         
         if sqlite3_bind_text(stmt, 7, Email, -1, nil) != SQLITE_OK {
             print("Error binding Email")
-            return;
+//            return;
         }
         
         if sqlite3_step(stmt) != SQLITE_DONE {
@@ -144,9 +145,7 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
             return
         }
         
-        print("everything is fine")
-        
-
+        readValues()
     }
     
 
@@ -157,6 +156,62 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
         
         self.dismiss(animated: true, completion: nil)
     }
+    
+    class Profile {
+        
+        var fName: String
+        var lName: String
+        var DOB: String
+        var gender: String
+        var weight: Int
+        var height: Int
+        var email: String
+        
+        init(fName: String, lName: String, DOB: String, gender: String, weight: Int, height: Int, email: String) {
+            
+            self.fName = fName
+            self.lName = lName
+            self.DOB = DOB
+            self.gender = gender
+            self.weight = weight
+            self.height = height
+            self.email = email
+            
+        }
+        
+    }
+    
+    func readValues() {
+        
+        ProfileList.removeAll()
+        
+        let queryString = "SELECT * FROM PROFILE"
+        
+        var stmt: OpaquePointer?
+        
+        if sqlite3_prepare(db, queryString, -1, &stmt, nil) != SQLITE_OK {
+            
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("error preparing insert: \(errmsg)")
+            return
+        }
+        
+        while(sqlite3_step(stmt) == SQLITE_ROW){
+            let fName = sqlite3_column_int(stmt, 0)
+            let lName = String(cString: sqlite3_column_text(stmt, 1))
+            let DOB = sqlite3_column_int(stmt, 2)
+            let gender = sqlite3_column_int(stmt, 3)
+            let weight = sqlite3_column_int(stmt, 4)
+            let height = sqlite3_column_int(stmt, 5)
+            let email = sqlite3_column_int(stmt, 6)
+            
+            ProfileList.append(Profile(fName: String(describing: fName), lName: String(describing: lName), DOB: String(describing: DOB), gender: String(describing: gender),                                 weight: Int(weight), height: Int(height), email: String(describing: email)))
+            
+            print(ProfileList)
+        }
+    }
+ 
+    
     
 }
 
